e228190486a29f712575a30ff4045b9f7b052f23
Derick A <alangiderick@gmail.com>
2021-08-20 13:08:27 +0100

    OrgInfo: Add OOC level 4, 5, 6, 7 into the form for M&E staff
    Change-Id: Icdb9a09c51f540c977d39263e6fc1c0e3f86bc84

--- src/wadpOrgInfoForm.js
+++ src/wadpOrgInfoForm.js
@@ -819,7 +819,11 @@
                         { data: '0' },
                         { data: '1' },
                         { data: '2' },
-                        { data: '3' }
+                        { data: '3' },
+                        { data: '4' },
+                        { data: '5' },
+                        { data: '6' },
+                        { data: '7' }
                     ]
                 } );
                 if ( this.out_of_compliance_level ) {
