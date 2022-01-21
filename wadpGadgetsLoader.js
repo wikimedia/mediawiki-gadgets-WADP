c74a46409bff3a07fa006e60c07057e7bccfde44
Derick Alangi <alangiderick@gmail.com>
2022-01-21 13:05:56 +0100

    GadgetsLoader: Add some dependency on loading for M&E staff accounts
    Change-Id: Ieaa3d0e6b38433132bcae233d644b03ace9238d8

--- wadpGadgetsLoader.js
+++ wadpGadgetsLoader.js
@@ -17,11 +17,10 @@
         var me_staff = [ 'DAlangi (WMF)', 'DNdubane (WMF)', 'MKaur (WMF)' ];
 
         if ( me_staff.indexOf( mw.config.values.wgUserName ) > -1 ) {
-            /* Load Organizational Info Form */
-            mw.loader.load( 'ext.gadget.wadpOrgInfoForm' );
-
-            /* Load the out of compliance checker */
-            mw.loader.load( 'ext.gadget.wadpOutOfComplianceChecker' );
+            /* Load OrgInfo Form: should load first before OOC checker */
+            mw.loader.using( 'ext.gadget.wadpOrgInfoForm', function () {
+                mw.loader.load( 'ext.gadget.wadpOutOfComplianceChecker' );
+            } );
         } else {
             /* Load Organizational Info Form for end users. */
             mw.loader.load( 'ext.gadget.wadpOrgInfoForm' );
