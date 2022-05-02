c64f1b18ee50ac5ebb479e20f931643d4518144e
Derick Alangi <alangiderick@gmail.com>
2022-05-02 14:33:48 +0100

    frontend: Don't prepend "User:" if it already exist infront username
    Change-Id: Id631592efb5c047aa3dbefdd06ac39ff3ab92cd8

--- src/frontend/wadpOrgInfoForm.js
+++ src/frontend/wadpOrgInfoForm.js
@@ -1527,18 +1527,18 @@
                                     );
                                 }
                                 if ( typeof membershipInfosManifest[ j ].group_contact1 !== 'undefined' &&
-                                    membershipInfosManifest[ j ].group_contact1.indexOf( "User:" ) !== -1
+                                    membershipInfosManifest[ j ].group_contact1.indexOf( "User:" ) === -1
                                 ) {
                                     insertInPlaceGC += generateKeyValuePair(
                                         'group_contact1',
-                                        membershipInfosManifest[ j ].group_contact1
+                                        'User:' + membershipInfosManifest[ j ].group_contact1
                                     );
                                 } else if ( typeof membershipInfosManifest[ j ].group_contact1 !== 'undefined' &&
                                     membershipInfosManifest[ j ].group_contact1
                                 ) {
                                     insertInPlaceGC += generateKeyValuePair(
                                         'group_contact1',
-                                        'User:' + membershipInfosManifest[ j ].group_contact1
+                                        membershipInfosManifest[ j ].group_contact1
                                     );
                                 } else {
                                     insertInPlaceGC += generateKeyValuePair(
@@ -1547,18 +1547,18 @@
                                     );
                                 }
                                 if ( typeof membershipInfosManifest[ j ].group_contact2 !== 'undefined' &&
-                                    membershipInfosManifest[ j ].group_contact2.indexOf( "User:" ) !== -1
+                                    membershipInfosManifest[ j ].group_contact2.indexOf( "User:" ) === -1
                                 ) {
                                     insertInPlaceGC += generateKeyValuePair(
                                         'group_contact2',
-                                        membershipInfosManifest[ j ].group_contact2
+                                        'User:' + membershipInfosManifest[ j ].group_contact2
                                     );
                                 } else if ( typeof membershipInfosManifest[ j ].group_contact2 !== 'undefined' &&
                                     membershipInfosManifest[ j ].group_contact2
                                 ) {
                                     insertInPlaceGC += generateKeyValuePair(
                                         'group_contact2',
-                                        'User:' + membershipInfosManifest[ j ].group_contact2
+                                        membershipInfosManifest[ j ].group_contact2
                                     );
                                 } else {
                                     insertInPlaceGC += generateKeyValuePair(
