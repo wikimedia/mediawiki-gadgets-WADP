353c85cff0e34571acf725e3864a1c2494c8e6a0
Simuli China <alice.j.simuli@gmail.com>
2022-10-03 06:43:24 +0300

    Sign talk page notification
    Change-Id: Ide601e761bccdfaa37bd2347a910fed01ad7bec8

--- src/frontend/wadpOutOfComplianceChecker.js
+++ src/frontend/wadpOutOfComplianceChecker.js
@@ -22,10 +22,18 @@
         postTalkPageNotification,
         getModuleContent,
         resetReportingDueDate,
+        dateFormatOptions,
         // If an affiliate's distance between its activity and financial report year
         // is less than the offset below, then the affiliate is considered compliant
         // provided that the activity report is the latest.
         AR_FR_YEAR_OFFSET = 2;
+
+    dateFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
 
     function init() {
         /**
@@ -334,7 +342,8 @@
                 "* Post your '''" + String( currentYear ) + "''' annual reporting to the meta via the  [[Wikimedia Affiliates Data Portal]] as soon as possible to return to compliance with your user group agreement.\n\n" +
                 "* Check that your group’s page is also up to date with past report links for historical record-keeping, and\n\n" +
                 "* Please send an email to [[Mailing_lists/Wikimedia_Announce|Wikimedia-l]] in order to share with a movement-wide audience.\n\n" +
-                "If you have any questions or need any further guidance, please don’t hesitate to reach out to wadportal{{at}}wikimedia.org.<br /><br />'''Best regards''', <br />''Wikimedia Affiliates Data Portal''\n\n";
+                "If you have any questions or need any further guidance, please don’t hesitate to reach out to wadportal{{at}}wikimedia.org.<br /><br />'''Best regards''', <br />''[[User:DNdubane_(WMF)|Dumisani Ndubane]]''\n\n" +
+                "<br />''Wikimedia Affiliates Data Portal''\n\n" + new Date().toLocaleDateString("en-US", dateFormatOptions) + "\n\n";
         };
 
         /**
@@ -378,7 +387,8 @@
             "* Post your '''" + String( currentYear ) + "''' annual reporting to the meta via the  [[Wikimedia Affiliates Data Portal]] as soon as possible to return to compliance with your user group agreement.\n\n" +
             "* Check that your group’s page is also up to date with past report links for historical record-keeping, and\n\n" +
             "* Please send an email to [[Mailing_lists/Wikimedia_Announce|Wikimedia-l]] in order to share with a movement-wide audience.\n\n" +
-            "If you have any questions or need any further guidance, please don’t hesitate to reach out to wadportal{{at}}wikimedia.org.<br /><br />'''Best regards''', <br />''Wikimedia Affiliates Data Portal''\n\n";
+            "If you have any questions or need any further guidance, please don’t hesitate to reach out to wadportal{{at}}wikimedia.org.<br /><br />'''Best regards''', <br />''[[User:DNdubane_(WMF)|Dumisani Ndubane]]''\n\n" +
+            "<br />''Wikimedia Affiliates Data Portal''\n\n" + new Date().toLocaleDateString("en-US", dateFormatOptions) + "\n\n";
         };
 
         /**
